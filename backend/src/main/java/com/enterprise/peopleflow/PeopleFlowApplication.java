af045dbcbb1a2107036fd8a6d0758561af6163bb
MusarratNaziaMd <musarratnazia02@gmail.com>
2026-07-28 15:08:49 +0530

    Resolved

--- backend/src/main/java/com/enterprise/peopleflow/PeopleFlowApplication.java
+++ backend/src/main/java/com/enterprise/peopleflow/PeopleFlowApplication.java
@@ -24,6 +24,7 @@
 
 package com.enterprise.peopleflow;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -31,6 +32,24 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PeopleFlowApplication {
 
     public static void main(String[] args) {
+
+        Dotenv dotenv = Dotenv.configure()
+                .directory("./")
+                .ignoreIfMissing()
+                .load();
+
+        dotenv.entries().forEach(entry -> {
+            System.setProperty(entry.getKey(), entry.getValue());
+        });
+
+        System.out.println("===== ENV TEST =====");
+
+        System.out.println("JWT_SECRET = " + System.getProperty("JWT_SECRET"));
+        System.out.println("JWT_EXPIRATION_MS = " + System.getProperty("JWT_EXPIRATION_MS"));
+        System.out.println("SPRING_DATASOURCE_URL = " + System.getProperty("SPRING_DATASOURCE_URL"));
+
+        System.out.println("====================");
+
         SpringApplication.run(PeopleFlowApplication.class, args);
     }
 }
--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:8080/api/v1',
+  baseURL: import.meta.env.VITE_API_BASE_URL || 'https://enterprise-hr-workforce-management-53oz.onrender.com/api/v1',
   headers: { 'Content-Type': 'application/json' },
 })
 
